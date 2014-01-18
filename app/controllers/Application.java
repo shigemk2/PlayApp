0f0d576f0b7d13ac2e3a6dd8974822336531bfc4
shigemk2 <i.am.shige@gmail.com>
2014-01-18 14:59:49 +0900

    フォーム作成(P37 図1.19)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,33 @@
 package controllers;
 
 import play.*;
+import play.data.*;
+import static play.data.Form.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render(
-                      "これは最初の値です。",
-                      "これは真ん中の引数です。",
-                      "これは最後のテストです。"));
+    // Form用の内部のクラス
+    public static class SampleForm {
+        public String message;
     }
 
+    // ルートにアクセスしたさいのAction
+    public static Result index() {
+        return ok(index.render("何か書いて", new Form(SampleForm.class)));
+    }
+
+    // sendにアクセスしたさいのAction
+    public static Result send() {
+        Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
+        if(!f.hasErrors()) {
+            SampleForm data = f.get();
+            String msg = "you typed: " + data.message;
+            return ok(index.render(msg, f));
+        } else {
+            return badRequest(index.render("ERROR", form(SampleForm.class)));
+        }
+    }
 }
