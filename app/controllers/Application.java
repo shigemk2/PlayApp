f2fbe17e250d274e061e30eed45109325c65b2a1
shigemk2 <i.am.shige@gmail.com>
2014-01-18 14:36:25 +0900

    Controllerからの値の受け渡し

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(
+                      "これは最初の値です。",
+                      "これは真ん中の引数です。",
+                      "これは最後のテストです。"));
     }
 
 }
